5096063d57d7fedadb785e19fe911651b7ed2c4a
AsithandileNyjah <atyuluu@gmail.com>
2023-11-21 10:28:33 +0000

    First commit

---
+++ JavaScript/index.js
@@ -0,0 +1,33 @@
+let minutes = document.querySelector('[minutes]').value
+
+let btn = document.querySelector('[multiply]')
+
+let btn2 = document.querySelector('[clear]')
+
+// constant second will always be 60
+const seconds = 60 
+
+let display = document.querySelector('[view]')
+
+// event listeners for both buttons
+btn.addEventListener('click', multiply)
+btn2.addEventListener('click', clear)
+
+// converting function to convert minutes to seconds
+
+function multiply (){
+    let minutes = document.querySelector('[minutes]').value
+    const seconds = 60
+    let ans = minutes * seconds
+    console.log(ans);
+    display.value = ans
+}
+// Function for the clear button, to clear both the answer and input for minutes
+function clear (){
+    document.querySelector('[minutes]').value = '';
+    document.querySelector('[view]').value = '';
+}
+
+function disableInput() {
+    document.querySelector('[view]').disabled = true;
+}
